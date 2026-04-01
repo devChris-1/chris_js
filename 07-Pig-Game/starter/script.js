e09504e677b7b3d46395a9c8384695b9e94e448c
Masterpitan <akoredeadetunji93@gmil.com>
2026-04-01 16:03:51 +0100

    testing git collab

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -7,7 +7,7 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-
+// Testing this git cloning and collaboration
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
